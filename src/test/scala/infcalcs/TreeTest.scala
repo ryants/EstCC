8a3997ca4fb0e0d1ec95241b2ff8e1f641491c90
John Bachman <bachmanjohn@gmail.com>
2014-05-24 23:05:09 -0400

    Docs and tests for Tree

---
+++ src/test/scala/infcalcs/TreeTest.scala
@@ -0,0 +1,47 @@
+package infcalcs
+
+import org.scalatest._
+import TreeDef._
+
+class TreeTest extends FlatSpec with Matchers {
+
+  "Binary tree helper functions" should
+    "be able to make an ordered list of nodes from a list of numbers" in {
+    val l = List(4.0, 3.0, 2.0, 1.0)
+    val orderedNodes = buildOrderedNodeList(l)
+    orderedNodes(0).value shouldBe Some(1.0)
+    orderedNodes(1).value shouldBe Some(2.0)
+    orderedNodes(2).value shouldBe Some(3.0)
+    orderedNodes(3).value shouldBe Some(4.0)
+  }
+
+  it should "find the index of the middle value of a list" in {
+    val l1 = List(1, 2, 3, 4, 5)
+    medSplit(l1)._1 shouldBe 2
+    medSplit(l1)._2 shouldBe List(1, 2)
+    medSplit(l1)._3 shouldBe List(4, 5)
+    val l2 = List(1, 2, 3, 4)
+    medSplit(l2)._1 shouldBe 1
+    medSplit(l2)._2 shouldBe List(1)
+    medSplit(l2)._3 shouldBe List(3, 4)
+  }
+
+  it should "correctly build a binary tree from an ordered list of nodes" in {
+    val l = List(4.0, 3.0, 2.0, 1.0)
+    val orderedNodes = buildOrderedNodeList(l)
+    val bt = buildTree(orderedNodes)
+    bt.value shouldBe Some(2.0)
+    bt.left.value shouldBe Some(1.0)
+    bt.right.value shouldBe Some(3.0)
+    bt.right.left.value shouldBe None
+    bt.right.right.value shouldBe Some(4.0)
+    bt.entries shouldBe 4
+    bt.right.entries shouldBe 2
+  }
+
+  it should "build an empty tree from an empty list" in {
+    val l = List()
+    val bt = buildTree(l)
+    bt.isEmpty shouldBe true
+  }
+}

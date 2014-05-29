58df6bc9fa435246203a1b321cff17630132c8af
John Bachman <bachmanjohn@gmail.com>
2014-05-28 21:33:12 -0400

    Docs for InfConfig.scala

--- src/main/scala/infcalcs/InfConfig.scala
+++ src/main/scala/infcalcs/InfConfig.scala
@@ -1,7 +1,12 @@
 package infcalcs
 
+/** Defines default values for the channel capacity calculation parameters.
+  *
+  * For more detailed descriptions of these parameters, see the README file.
+  */
 object InfConfig {
 
+  /** Parameters that have list values. */
   val listParams: Map[String, Option[List[Double]]] =
     Map("columnPair" -> Some(List(0.0, 1.0)),
     "responseValues" -> None,
@@ -11,6 +16,7 @@ object InfConfig {
     "sampleFractions" -> Some((0.8 to 0.975 by 0.025).toList),
     "biPeakWeights" -> Some(List(0.4, 0.5, 0.6)))
 
+  /** Parameters that have numerical values. */
   val numParams: Map[String, Int] =
     Map("numRandom" -> 10,
     "numForCutoff" -> 1,
@@ -21,11 +27,13 @@ object InfConfig {
     "biSigmaNumber" -> 5,
     "repsPerFraction" -> 20)
 
+  /** Parameters that have string values. */
   val stringParams: Map[String, String] =
     Map("directory" -> "./",
     "filePrefix" -> "out",
     "logSpace" -> "false")
 
+  /** Instance of [[Parameters]] containing default parameter values. */
   val defaultParameters: Parameters = (listParams, numParams, stringParams)
 
 }
